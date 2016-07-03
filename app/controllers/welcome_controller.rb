21bf1baeb9fe07e3ff4da0cb277d14ec954b23e0
Thomas Charbonnel <thomas@charbonnel.eu>
2016-07-03 15:17:13 +0200

    Starting Front-end Integration
    Website's menu is done.
    Main parts were added.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -1,4 +1,10 @@
 class WelcomeController < ApplicationController
   def index
   end
+
+  def who_are_we
+  end
+
+  def innovate_together
+  end
 end
--- config/routes.rb
+++ config/routes.rb
@@ -12,6 +12,8 @@ Rails.application.routes.draw do
   }
 
   get 'welcome/index'
+  get 'welcome/who_are_we' => 'welcome#who_are_we'
+  get 'welcome/innovate_together' => 'welcome#innovate_together'
 
   # The priority is based upon order of creation: first created -> highest priority.
   # See how all your routes lay out with "rake routes".
