096e7c5b84eff61c29f67bcaa7328cb54a4ed727
Thomas Charbonnel <thomas@charbonnel.eu>
2016-08-25 19:07:26 +0200

    Add password changing option in profile

--- app/controllers/profiles_controller.rb
+++ app/controllers/profiles_controller.rb
@@ -5,7 +5,7 @@ class ProfilesController < ApplicationController
 
   def edit
     @user = User.find_by(id: params[:id])
-    if @user != current_user and not current_user.admin?
+    if current_user and @user != current_user and not current_user.admin?
       flash[:error] = "Vous n'avez pas les droits nécessaires pour éditer ce profil."
       render 'show'
     end
@@ -17,12 +17,18 @@ class ProfilesController < ApplicationController
 
   def update
     @user = User.find_by(id: params[:id])
-    if (current_user == @user or current_user.admin?) and @user.update_attributes(user_params)
-      flash[:success] = 'Profil mis à jour.'
+
+    if current_user and (current_user == @user or current_user.admin?)
+      if not user_params[:password].blank? and @user.update_with_password(user_params) or @user.update_without_password(user_params)
+        flash[:success] = "Profil mis à jour."
+        sign_in @user, :bypass => true
+      else
+        flash[:error] = "Une erreur est survenue lors de l'édition du profil : #{@user.errors.full_messages}"
+      end
       render 'edit'
     else
-      flash[:error] = 'Une erreur est survenue lors de l\'édition du profil.'
-      render 'edit'
+      flash[:error] = "Vous n'avez pas les droits suffisants pour poursuivre."
+      render 'index'
     end
   end
   
