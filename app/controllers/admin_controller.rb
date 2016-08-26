ee3efbcfd6f36a0a6552e877a1151dd8458622f2
Thomas Charbonnel <thomas@charbonnel.eu>
2016-08-26 17:33:35 +0200

    Put password first in CSV batch processing

--- app/controllers/admin_controller.rb
+++ app/controllers/admin_controller.rb
@@ -5,14 +5,14 @@ class AdminController < ApplicationController
   before_action :verify_is_admin
 
   def batch_processing
-    user_keys = [:lastname, :firstname, :email, :promotion, :password]
+    user_keys = [:password, :lastname, :firstname, :email, :promotion]
     @results = []
 
     if params[:csv_content]
       begin
         User.transaction do # Let's make it a transaction so that every record is discarded if the CSV is invalid.
           CSV.parse(params[:csv_content]) do |row|
-            @results << User.create!(Hash[user_keys.zip(row + [SecureRandom.urlsafe_base64])])
+            @results << User.create!(Hash[user_keys.zip([SecureRandom.urlsafe_base64] + row)])
           end
         end
       rescue ActiveRecord::RecordInvalid => e
