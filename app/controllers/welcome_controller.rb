443e6ccb9e2e64f73b5b7d327c21bdfe87715c13
Thomas Charbonnel <thomas@charbonnel.eu>
2016-08-27 23:01:18 +0200

    Ok, I'm tired, too bad heroku doesn't support &.

--- app/controllers/welcome_controller.rb
+++ app/controllers/welcome_controller.rb
@@ -2,14 +2,14 @@ require 'ostruct'
 
 class WelcomeController < ApplicationController
   def index
-    @setting = (Settings.find_by(key: 'home_page') || OpenStruct.new).value
+    @setting = (Settings.find_by(key: 'home_page') || OpenStruct.new).value || " "
   end
 
   def who_are_we
-    @setting = (Settings.find_by(key: 'who_are_we') || OpenStruct.new).value
+    @setting = (Settings.find_by(key: 'who_are_we') || OpenStruct.new).value || " "
   end
 
   def innovate_together
-    @setting = (Settings.find_by(key: 'innovate_together') || OpenStruct.new).value
+    @setting = (Settings.find_by(key: 'innovate_together') || OpenStruct.new).value || " "
   end
 end
