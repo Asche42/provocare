b02b6da8f77470fccde1652f8220fe4ff84a3dd9
Thomas Charbonnel <thomas@charbonnel.eu>
2017-02-21 15:25:30 +0800

    fix: articles not displaying when unlogged

--- app/helpers/application_helper.rb
+++ app/helpers/application_helper.rb
@@ -1,5 +1,5 @@
 module ApplicationHelper
   def can_edit_item?(item)
-    current_user == item.user or current_user.admin?
+    current_user and (current_user == item.user or current_user.admin?)
   end
 end
